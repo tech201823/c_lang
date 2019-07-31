839e06187e016f18b71f50af74f9028ad0d2bf17
tech201823 <tech201823@gmail.com>
2019-07-31 16:54:49 +0900

    first practice commit

---
+++ hello.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+// int main(void){
+
+//     int x = 10;
+//     float y = 5.2;
+//     char c = 'A';
+
+//     printf("x = %d ,y = %f,c = %c \n",x,y,c);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(void){
+
+//     int x = 10 % 3;
+//     x ++;
+
+//     printf("%d\n",x);
+
+//     return 0;
+// }
+
+// dotinstall 4
